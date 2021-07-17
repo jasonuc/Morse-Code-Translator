6603b86f4c5edc5b08a23647a9320c829e32711b
jumahi <mrjasonu@gmail.com>
2021-07-17 23:12:29 +0100

    Main.py fully edited

---
+++ main.py
@@ -0,0 +1,17 @@
+from morsecode import morsecode as code
+
+def english_to_morse(string):
+    string = str(string).upper()
+    morse_code_string = ''
+    for char in string:
+        if char in list(code.keys()):
+            morse_code_string+= code[char]
+        else:
+            continue
+    morse_file = open('Morse-Code-Fall.txt','w')
+    morse_file.write(morse_code_string)
+    morse_file.close()
+    print("THE TEXT HAS BEEN WRITTEN INTO Morse-Code-Fall.txt IN MORSE CODE.")
+
+
+english_to_morse('Hello world!')
