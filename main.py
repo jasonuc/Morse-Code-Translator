a84c6fae15ac77e7f11d4840959f57e1d6f245a3
jumahi <mrjasonu@gmail.com>
2021-07-19 11:43:27 +0100

    Software can now change from morse code to english

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ def english_to_morse(string):
     for char in string:
         if char in list(code.keys()):
             morse_code_string+= code[char]
+            morse_code_string += '/'
         else:
             continue
-    morse_file = open('Morse-Code-Fall.txt','w')
+    morse_file = open('Morse-Code-File.txt','w')
     morse_file.write(morse_code_string)
     morse_file.close()
     print("THE TEXT HAS BEEN WRITTEN INTO Morse-Code-Fall.txt IN MORSE CODE.")
+    return morse_code_string
 
 def morse_to_enlish(string):
-    pass
+    print('Please seperate each character witha space')
+    dic_of_morsecode = {}
+    n_string = ''
+    for key, value in code.items():
+        dic_of_morsecode[value] = key
+    for group in string.split('/'):
+        n_string +=  str(dic_of_morsecode[group])
+    english_file = open('English-File.txt','w')
+    english_file.write(n_string)
+    english_file.close()
+    print("THE TEXT HAS BEEN WRITTEN INTO English-File.txt IN MORSE CODE.")
+    return n_string
 
-english_to_morse('He fumbled in the darkness looking for the light switch, but when he finally found it there was someone already there.')
+english_to_morse('Jam Fish')
+print(morse_to_enlish('•---/•-/--/|/••-•/••/•••/••••/'))
